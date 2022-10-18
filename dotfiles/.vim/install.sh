2095b1c206f47f4a67b18ceca803482fe16c2c11
Muhammed.m.Abdelkader <muhammed.m.abdelkader@gmail.com>
2022-10-18 02:37:19 +0200

    feat(dotFiles):add dot files

---
+++ dotfiles/.vim/install.sh
@@ -0,0 +1,4 @@
+#! /bin/bash 
+
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp ./vimrc $HOME/.vimrc 
---
+++ toolsInstallation/kali.sh
@@ -0,0 +1,7 @@
+#! /bin/bash 
+
+
+docker pull kalilinux/kali-rolling
+docker run -ti kalilinux/kali-rolling /bin/bash
+
+
